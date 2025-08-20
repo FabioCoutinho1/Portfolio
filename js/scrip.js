95ed26fda5b5a0f6c3e8ad7335764d675163c407
FabioCoutinho <fabiofarias549@gmail.com>
2025-08-20 12:10:24 -0300

    adjustments to semantics and some styles

--- js/scrip.js
+++ js/scrip.js
@@ -1,22 +1,23 @@
 const mobileMenu = document.querySelector(".mobile__list");
 const menuBtn = document.querySelector("#btn-menu");
-const switchTheme = document.querySelector("#switch");
+const switchTheme = document.querySelectorAll(".switch__input");
 const savedTheme = localStorage.getItem("theme");
 
-switchTheme.addEventListener("change", () => {
-  const isDarkMode = switchTheme.checked;
-  const currentTheme = document.documentElement.getAttribute("data-theme");
+switchTheme.forEach((el) => {
+  el.addEventListener("change", () => {
+    const isDarkMode = el.checked;
+    const currentTheme = document.documentElement.getAttribute("data-theme");
 
-  if (!isDarkMode && currentTheme === "light") {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-  }
-  console.log(isDarkMode);
+    if (!isDarkMode && currentTheme === "light") {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
+    }
+    console.log(isDarkMode);
+  });
 });
-
 menuBtn.addEventListener("click", () => {
   const isOpen = mobileMenu.classList.toggle("show");
   menuBtn.classList.toggle("fa-bars", !isOpen);
@@ -26,7 +27,7 @@ menuBtn.addEventListener("click", () => {
 if (savedTheme) {
   document.documentElement.setAttribute("data-theme", savedTheme);
   console.log(`Tema carregado: ${savedTheme}`);
-  savedTheme === "light"
-    ? (switchTheme.checked = true)
-    : (switchTheme.checked = false);
+  switchTheme.forEach((el) => {
+    savedTheme === "light" ? (el.checked = true) : (el.checked = false);
+  });
 }
