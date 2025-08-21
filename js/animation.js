e0f4ea9ded8041802e0741d1cd2736d83eb39dc2
FabioCoutinho <fabiofarias549@gmail.com>
2025-08-21 16:20:14 -0300

    add observer animation

---
+++ js/animation.js
@@ -0,0 +1,24 @@
+const containerLeft = document.querySelector(".container__left");
+const animations = document.querySelectorAll(".observer__animation");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+animations.forEach((animation) => {
+  observer.observe(animation);
+});
+
+console.log(home);
